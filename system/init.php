046db427df1f465c59f9929030e2223b6acd020f
Joshua Kidd <kidd.josh.343@gmail.com>
2013-03-16 18:48:38 -0500

    init global file
    added some classes

--- system/init.php
+++ system/init.php
@@ -1,9 +1,12 @@
 <?php
+session_start();
 
 include_once('class/db.class.php');
 include_once('class/user.class.php');
+include_once('class/site.class.php');
 
 $db = new db;
 $user = new user;
+$site = new site;
 
 ?>
