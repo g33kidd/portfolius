d86f461e3937907ed7c6714d061faa4b09044346
Geekahz <GeekFlyingHigh@gmail.com>
2013-03-22 07:33:45 +0000

    some changes

---
+++ design/theme/cover/options.php
@@ -0,0 +1,14 @@
+<?php
+
+$options = array(
+	'twitter_username' => array(
+		'type' => 'text',
+		'label' => 'Twitter Username'
+	),
+	'facebook_username' => array(
+		'type' => 'text',
+		'label' => 'Facebook Username'
+	)
+);
+
+?>
