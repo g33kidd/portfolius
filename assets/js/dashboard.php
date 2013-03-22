ea7984ba9112e2a6c9a1f88651bd42fc7ad911bc
Joshua Kidd <kidd.josh.343@gmail.com>
2013-03-21 23:22:31 -0500

    added dashboard templates

---
+++ assets/js/dashboard.php
@@ -0,0 +1,4 @@
+<?php session_start(); header("content-type: application/x-javascript"); ?>$(document).ready(function() {		//alert("something for <? echo $_SESSION['fullname']; ?>");
+	<? // Some PHP comment ?>
+	
+}); 
