e84f545ca3d563527d9ed0c3b50218c04e973b31
Geekahz <GeekFlyingHigh@gmail.com>
2013-03-29 06:09:51 +0000

    CSS Changed, Landing page added
    Awaiting clean up

--- dashboard.php
+++ dashboard.php
@@ -1,50 +1,31 @@
-<?php 
-
-include_once("system/init.php"); 
-$sites = $user->site_count($_SESSION['id']);
-
+<?php
+include_once ("system/init.php");
+$sites = $user -> site_count($_SESSION['id']);
 ?>
 <!doctype html>
 <html lang="en">
-	
-	<head>
-		<meta charset="UTF-8" />
-		<title>Portfolius Dashboard &mdash; create cool personal portfolio sites easy.</title>
-		<link rel="stylesheet" href="assets/css/main.css" type="text/css" media="screen" title="no title" charset="utf-8"/>
-		<link rel="stylesheet" href="assets/css/dashboard.css" type="text/css" media="screen" title="no title" charset="utf-8"/>
-	</head>
-	
-	<body>
-		
-		<div class="topbar">
-			<div class="padding">
-				<div class="container">
-					<img src="assets/images/icons/partial.svg" class="logo left" width="25px" height="25px">
-					<div class="title left">Portfolius</div>
-				</div>
-				<div class="clear"></div>
-			</div>
-		</div>
-		<div class="topbar-space"></div>
-		
-		<div class="container">
-			<div class="dashMain">
-				<?php
-				
-				if($sites < 1) {
-					echo "YOU ARE A FUCKING NIGGER!";
-				}else{
-					echo "YOU ARE STILL A FUCKING NIGGER!";
-				}
-				
-				?>
-			</div>
-		</div>
-		
-		<script src="//code.jquery.com/jquery-latest.js"></script>
-		<script src="assets/js/dashboard.php"></script>
-		<script src="assets/js/site.js"></script>
-		
-	</body>
-	
+ <head>
+  <meta charset="UTF-8" />
+  <title>Portfolius Dashboard &mdash; create cool personal portfolio sites easy.</title>
+  <link rel="stylesheet" href="assets/css/base.css" type="text/css" media="screen" title="no title" charset="utf-8"/>
+  <link rel="stylesheet" href="assets/css/dashboard.css" type="text/css" media="screen" title="no title" charset="utf-8"/>
+ </head>
+ <body>
+  <div id="globalContainer">
+   <div id="c-PUBLIC-navbar" class="hasRel">
+    <nav class="nav-inner">
+     <div class="container">
+      <ul>
+       <li class="logo"><img src="assets/images/icons/partial.svg" width="25px" height="25px"></li>
+       <li class="portfolius">Portfolius</li>
+      </ul>
+     </div>
+    </nav>
+   </div>
+   <div id="contentContainer"><b>I am some text</b></div>
+  </div>
+  <script src="//code.jquery.com/jquery-latest.js"></script>
+  <script src="assets/js/dashboard.php"></script>
+  <script src="assets/js/site.js"></script>
+ </body>
 </html>
--- index.php
+++ index.php
@@ -1,54 +1,75 @@
 <?php
-
-require_once("system/init.php");
-
-session_destroy();
-
-if(is_user_loggedin()) 
-	header("Location: dashboard.php");
-
+	require_once("system/init.php");
+	session_destroy();
+	if(is_user_loggedin()) header("Location: dashboard.php");
 ?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta http-equiv="Content-type" content="text/html; charset=utf-8"/>
-		<title>Portfolius &mdash; create a simple website fast.</title>
-		<link rel="stylesheet" href="assets/css/main.css" type="text/css">
-	</head>
-	
-	<body>
-		
-		<h1 class="landingtitle">Portfolius _</h1>
-		<div class="container">
-			<div class="section left">
-				<h1 class="title">Sign Up<span>Stay in touch</span></h1>
-				<hr>
-				<form action="" id="register">
-					<input type="text" name="firstname" placeholder="First Name" id="firstName" />
-	                <input type="text" name="lastname" placeholder="Last Name" id="lastName" />
-	                <input type="email" name="email" placeholder="Email Adress" id="emailAdress" />
-	                <input type="password" name="password" placeholder="Password" id="password" />
-	                <input type="hidden" value="register" name="request" />
-	                <br><input type="submit" value="Register">
-				</form>
-				<br>
-				<h1 class="title">Login<span></span></h1>
-				<hr>
-				<form action="" id="login">
-					<input type="email" name="email" placeholder="Email Adress" id="emailAdress" />
-	                <input type="password" name="password" placeholder="Password" id="password" />
-	                <input type="hidden" value="login" name="request" />
-	                <br><input type="submit" value="Register">
-				</form>
-			</div>
-			
-			<div class="section right">
-				Some about text will here.
-			</div>
-		</div>
-		<script src="//code.jquery.com/jquery-latest.js"></script>
-		<script src="assets/js/site.js"></script>
-		
-	</body>
-	
+<!DOCTYPE HTML>
+<html lang="en">
+ <head>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1" />
+  <title>Portfolius | Home</title>
+  <meta name="description" content="" />
+  <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+  <link rel="stylesheet" type="text/css" href="assets/css/base.css" media="screen" title="no title" charset="utf-8" />
+  <link rel="stylesheet" type="text/css" href="assets/css/home.css" media="screen" title="no title" charset="utf-8" />
+ </head>
+ <body>
+  <!-- :start navigation bar -- top of page, faded for effect -->
+  <div id="c-PUBLIC-navbar" class="hasRel">
+   <nav class="nav-inner">
+   	<div class="container">
+   	
+   		<ul>
+   		 <li class="logo"><img src="assets/images/icons/partial.svg" width="25px" height="25px"></li>
+   		 <li class="portfolius">Portfolius</li>
+   		</ul>
+   		
+   	</div>
+   </nav>
+   
+  </div>
+  <!-- :end navigation bar -->
+  <!-- :start overlay --><div id="overlay" class="s-private"></div><!-- :end overlay -->
+  <!-- :start top section of landing page -->
+  
+  <section id="landing" class="top content-lander">
+  	<h1 class="mtl">This is Portfolius</h1>
+  </section>
+  
+  <!-- :end top section of landing page -->
+  
+  <!-- :start content container for landing page -->
+  
+  <section id="landing" class="content">
+  	<div class="container example">
+	<div class="d-data-private-cover">
+	<div class="image-container"><img src="assets/images/landing/cover.jpg" alt="cover image for portfolius landing page" class="cover"></div>
+	 <div class="user-data-example">
+	  <div class="data-container">
+	   <span class="user-data-title">Zachary Kidd</span>
+	   <span class="user-data-subtitle">Artist, Musician, Rocker</span>
+	  </div>
+	  <!-- <div class="c-PUBLIC-profile-img"></div> -->
+	 </div>
+	</div>
+	<div class="z-temp-home">
+	 <span>Set your stage</span>
+	</div>
+	<div class="clear"></div>
+	</div>
+	<!-- :todo: page jumping nav bar -->
+	<nav id="uinavblock">
+	 <ul>
+	  <li>Hello</li>
+	  <li>Bellow</li>
+	  <li>Slowly</li>
+	  <li>Hello</li>
+	  <li>Bellow</li>
+	  <li>Slowly</li>
+	 </ul>
+	</nav>
+  </section>
+  
+  <!-- :end content container for landing page -->
+ </body>
 </html>
